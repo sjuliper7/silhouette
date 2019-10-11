96c59282694a21eb3b57cb44662ff3835a2b333b
sjuliper7 <sjuliper7@gmail.com>
2019-10-11 11:30:14 +0700

    update grpc

--- services/user-service/delivery/grpc.go
+++ services/user-service/delivery/grpc.go
@@ -1,10 +1,6 @@
 package delivery
 
 import (
-	"context"
-
-	"github.com/golang/protobuf/ptypes/empty"
-	"github.com/sjuliper7/silhouette/common/models"
 	"github.com/sjuliper7/silhouette/services/user-service/usecase"
 )
 
@@ -13,25 +9,7 @@ type UserServer struct {
 	usecase usecase.UserUsecase
 }
 
+//NewUserServer is function to implement usecase interface
 func NewUserServer(uc usecase.UserUsecase) UserServer {
 	return UserServer{uc}
-}
-
-func (us UserServer) List(context.Context, *empty.Empty) (*models.UserList, error) {
-	var users *models.UserList
-
-	users.List = make([]*models.User, 0)
-
-	uu := us.usecase.GetAlluser()
-
-	for _, u := range uu {
-		var user *models.User
-		user.Id = u.ID
-		user.Name = u.Name
-		user.LastName = u.LastName
-
-		users.List = append(users.List, user)
-	}
-
-	return users, nil
 }
