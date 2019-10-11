927f7bb15dbc6cdb52049d1cbdaaf2cad2c1d7a2
sjuliper7 <sjuliper7@gmail.com>
2019-10-11 10:35:54 +0700

    create function populateStringConnection

--- services/user-service/config/database.go
+++ services/user-service/config/database.go
@@ -3,13 +3,14 @@ package config
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 //InitDatabase is function to create connection to Database
 func (cf *Config) initDatabase() {
-	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/practice")
+	db, err := sql.Open("mysql", populateStringConnection())
 
 	if err != nil {
 		log.Fatal(err)
@@ -19,4 +20,14 @@ func (cf *Config) initDatabase() {
 
 	cf.DB = db
 
+}
+
+func populateStringConnection() string {
+	stringConnection := ""
+
+	stringConnection += os.Getenv("DATABASE_USER") + ":" + os.Getenv("DATABASE_PASSWORD") +
+		"@tcp(" + os.Getenv("DATABASE_HOST") + ":" + os.Getenv("DATABASE_PORT") + ")/" +
+		os.Getenv("DATABASE_NAME")
+
+	return stringConnection
 }
