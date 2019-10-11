5dd93087067a3e3fe3f2d76c3c4e42d6ee32ec9d
sjuliper7 <sjuliper7@gmail.com>
2019-10-11 12:42:16 +0700

    completing client to test grpc delivery

--- client/main.go
+++ client/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/sjuliper7/silhouette/common/config"
+	"github.com/sjuliper7/silhouette/common/models"
+	"google.golang.org/grpc"
+	"log"
+)
 
 func main() {
 	fmt.Println("Hello World i'm Client")
+
+	userService := connectToUserService()
+
+	result, err := userService.List(context.Background(), new(empty.Empty))
+	if err != nil {
+		log.Println("[userService][List] Failed when remote function %+v", err)
+	}
+	fmt.Println(result)
+}
+
+func connectToUserService() models.UsersClient {
+	userPort := config.SERVICE_USER_PORT
+	conn, err := grpc.Dial(userPort, grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatal("could not connect to", userPort, err)
+	}
+
+	return models.NewUsersClient(conn)
 }
