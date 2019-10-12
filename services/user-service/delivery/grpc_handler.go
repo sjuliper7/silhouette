d63414ed7afa4c29dc91115950fe4c4e09aee748
sjuliper7 <sjuliper7@gmail.com>
2019-10-12 18:37:09 +0700

    completing handler gRPC

--- services/user-service/delivery/grpc_handler.go
+++ services/user-service/delivery/grpc_handler.go
@@ -4,23 +4,30 @@ import (
 	"context"
 	"github.com/golang/protobuf/ptypes/empty"
 	"github.com/sjuliper7/silhouette/common/models"
+	"log"
 )
 
 func (us UserServer) List(context.Context, *empty.Empty) (*models.UserList, error) {
-	var users *models.UserList
+	var userList models.UserList
+	var users []*models.User
 
-	users.List = make([]*models.User, 0)
+	uu, err := us.usecase.GetAlluser()
 
-	uu := us.usecase.GetAlluser()
+	if err != nil {
+		log.Println("Failed when call [usecase][GetAlluser]")
+		return nil, err
+	}
 
 	for _, u := range uu {
-		var user *models.User
+		var user models.User
 		user.Id = u.ID
 		user.Name = u.Name
 		user.LastName = u.LastName
 
-		users.List = append(users.List, user)
+		users = append(users, &user)
 	}
 
-	return users, nil
+	userList.List = users
+
+	return &userList, nil
 }
