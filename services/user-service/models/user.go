5947804e7a3ec6f4b473ae5bdc4e3650cba77d11
sjuliper7 <sjuliper7@gmail.com>
2019-10-14 18:44:12 +0700

    update attribute

--- services/user-service/models/user.go
+++ services/user-service/models/user.go
@@ -3,6 +3,8 @@ package models
 //User struct is represntative form table user
 type User struct {
 	ID       uint64 `json:"id" db:"id"`
+	Username string `json:"username" db:"username"`
+	Email    string `json:"email" db:"email"`
 	Name     string `json:"name" db:"name"`
-	LastName string `json:"last_name" db:"last_name"`
+	Role     string `json:"role" db:"role"`
 }
