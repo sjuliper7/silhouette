986fdb1c2621cadcd42d8c92fa8ca8756653d9ed
sjuliper7 <sjuliper7@gmail.com>
2019-10-15 11:38:57 +0700

    implement addUser and getUser

--- services/user-service/repositories/user_repository.go
+++ services/user-service/repositories/user_repository.go
@@ -39,3 +39,38 @@ func (repo mysqlRepository) GetAlluser() (users []models.User, err error) {
 
 	return users, nil
 }
+
+func (repo mysqlRepository) AddUser(user *models.User) (err error) {
+	sql := `INSERT INTO users(username, email, name, role) VALUES(?, ?, ?, ?)`
+
+	result := repo.Conn.MustExec(sql, user.Username, user.Email, user.Name, user.Role)
+	var temp int64
+	temp, err = result.LastInsertId()
+	if err != nil {
+		log.Println("Error when inserting values ", err)
+		return err
+	}
+
+	user.ID = uint64(temp)
+
+	return nil
+}
+
+func (repo mysqlRepository) GetUser(userID int64) (user models.User, err error) {
+
+	sql := `SELECT id, username, email, name, role FROM users where id = ?`
+
+	stmt, err := repo.Conn.Preparex(sql)
+
+	if err != nil {
+		log.Println("Error when prepare the query %v", err)
+	}
+
+	err = stmt.Get(&user, userID)
+
+	if err != nil {
+		log.Println("Error when getting the value %v", err)
+	}
+
+	return user, nil
+}
