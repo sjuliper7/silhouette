280940d397fd3b60263f154ce2b46688a015a965
sjuliper7 <sjuliper7@gmail.com>
2019-10-15 11:37:08 +0700

    add function post and getUser

--- services/user-service/delivery/rest_handler.go
+++ services/user-service/delivery/rest_handler.go
@@ -1,7 +1,11 @@
 package delivery
 
 import (
+	"github.com/gorilla/mux"
+	"github.com/sjuliper7/silhouette/services/user-service/models"
+	"log"
 	"net/http"
+	"strconv"
 )
 
 func (usr UserServerRest) fetchUser(w http.ResponseWriter, r *http.Request) {
@@ -11,4 +15,38 @@ func (usr UserServerRest) fetchUser(w http.ResponseWriter, r *http.Request) {
 	}
 
 	respondWithJSON(w, http.StatusOK, users)
+}
+
+func (usr UserServerRest) postUser(w http.ResponseWriter, r *http.Request) {
+	var user = models.User{
+		Username: r.FormValue("username"),
+		Email:    r.FormValue("email"),
+		Name:     r.FormValue("name"),
+		Role:     r.FormValue("role"),
+	}
+
+	err := usr.usecase.AddUser(&user)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+	}
+
+	respondWithJSON(w, http.StatusOK, user)
+}
+
+func (usr UserServerRest) getUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Println("Error when casting params to int")
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+	}
+
+	user, err := usr.usecase.GetUser(int64(id))
+
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+	}
+
+	respondWithJSON(w, http.StatusOK, user)
 }
