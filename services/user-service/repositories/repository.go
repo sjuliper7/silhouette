2306ae2564488dd7a05345be083de3dedd0d5a08
sjuliper7 <sjuliper7@gmail.com>
2019-10-15 11:37:54 +0700

    add function adduser and getuser

--- services/user-service/repositories/repository.go
+++ services/user-service/repositories/repository.go
@@ -7,4 +7,6 @@ import (
 //Repository declaration type interface
 type Repository interface {
 	GetAlluser() (users []models.User, err error)
+	AddUser(user *models.User) (err error)
+	GetUser(userID int64) (user models.User, err error)
 }
