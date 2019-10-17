43f3fe0633da4e955f52b4b15ab22ad935ff6bbf
sjuliper7 <sjuliper7@gmail.com>
2019-10-17 11:56:22 +0700

    update build file proto

--- common/protocs/profile.pb.go
+++ common/protocs/profile.pb.go
@@ -199,25 +199,25 @@ func init() {
 func init() { proto.RegisterFile("profile.proto", fileDescriptor_744bf7a47b381504) }
 
 var fileDescriptor_744bf7a47b381504 = []byte{
-	// 280 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xcc, 0x91, 0xc1, 0x4e, 0xb3, 0x40,
-	0x14, 0x85, 0xff, 0xa1, 0x7f, 0xa1, 0x5e, 0x53, 0x63, 0x66, 0xa1, 0x13, 0xdc, 0x10, 0x56, 0xac,
-	0x68, 0xa2, 0x4f, 0x80, 0xc1, 0x34, 0x6c, 0x4c, 0x43, 0xd2, 0xb8, 0x96, 0xce, 0x2d, 0x36, 0x02,
-	0x43, 0x66, 0x06, 0x13, 0x1f, 0xc6, 0x47, 0xf0, 0x01, 0x7c, 0x3b, 0x33, 0xc3, 0x58, 0xdd, 0xb8,
-	0x62, 0xe3, 0x8a, 0x7c, 0xf7, 0xdc, 0x73, 0xb8, 0x27, 0x03, 0xcb, 0x5e, 0x8a, 0xfd, 0xa1, 0xc1,
-	0xb4, 0x97, 0x42, 0x0b, 0x1a, 0xd8, 0xcf, 0x4e, 0x85, 0x57, 0xb5, 0x10, 0x75, 0x83, 0x2b, 0xcb,
-	0xd5, 0xb0, 0x5f, 0x61, 0xdb, 0xeb, 0xd7, 0x71, 0x2b, 0xfe, 0x20, 0x10, 0x6c, 0x46, 0x1f, 0x3d,
-	0x03, 0xaf, 0xc8, 0x19, 0x89, 0x48, 0x32, 0x2b, 0xbd, 0x22, 0xa7, 0x17, 0xe0, 0x6f, 0x15, 0xca,
-	0x22, 0x67, 0x9e, 0x9d, 0x39, 0xa2, 0x0c, 0x82, 0x8c, 0x73, 0x89, 0x4a, 0xb1, 0x59, 0x44, 0x92,
-	0x93, 0xf2, 0x0b, 0x8d, 0xe3, 0x41, 0xc8, 0xe7, 0x4c, 0xb3, 0xff, 0x56, 0x70, 0x44, 0x23, 0x38,
-	0xdd, 0x3c, 0x89, 0x0e, 0xef, 0x87, 0xb6, 0x42, 0xc9, 0xe6, 0x56, 0xfc, 0x39, 0x32, 0xce, 0x35,
-	0x76, 0x1c, 0x25, 0xf3, 0x47, 0xe7, 0x48, 0x34, 0x84, 0x45, 0xa1, 0xb2, 0x9d, 0x3e, 0xbc, 0x20,
-	0x0b, 0x22, 0x92, 0x2c, 0xca, 0x23, 0xc7, 0xef, 0x04, 0x2e, 0xcd, 0x49, 0x6b, 0xd4, 0xae, 0x42,
-	0x26, 0xeb, 0xa1, 0xc5, 0x4e, 0xab, 0xbf, 0xd8, 0xe5, 0xfa, 0xcd, 0x83, 0xb9, 0xf9, 0xad, 0xa2,
-	0xb7, 0x00, 0xdf, 0x47, 0xd3, 0x28, 0x75, 0x4f, 0x95, 0xfe, 0xd2, 0x26, 0x3c, 0x3f, 0x6e, 0x38,
-	0x29, 0xfe, 0x67, 0x32, 0x32, 0xce, 0xa7, 0x65, 0xdc, 0xc1, 0x72, 0xdb, 0xf3, 0x47, 0x8d, 0x93,
-	0x63, 0x72, 0x6c, 0x70, 0x62, 0x4c, 0xe5, 0xdb, 0xd1, 0xcd, 0x67, 0x00, 0x00, 0x00, 0xff, 0xff,
-	0x9c, 0xa0, 0x72, 0x65, 0xc9, 0x02, 0x00, 0x00,
+	// 278 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xcc, 0x91, 0x41, 0x4a, 0xc4, 0x30,
+	0x14, 0x86, 0x4d, 0x47, 0xa7, 0xf5, 0xc9, 0x88, 0x64, 0xa1, 0xa1, 0x6e, 0x4a, 0x57, 0x5d, 0x75,
+	0x40, 0x4f, 0x10, 0xa9, 0x0c, 0xdd, 0xc8, 0x50, 0x18, 0x5c, 0xdb, 0xc9, 0x9b, 0x3a, 0xd8, 0x36,
+	0x25, 0x49, 0x05, 0x6f, 0xe3, 0x05, 0x3c, 0x80, 0xb7, 0x93, 0xa6, 0x71, 0x74, 0xe3, 0xaa, 0x1b,
+	0x57, 0xe1, 0x7b, 0xef, 0xfd, 0x3f, 0xff, 0x4f, 0x60, 0xd1, 0x29, 0xb9, 0xdb, 0xd7, 0x98, 0x76,
+	0x4a, 0x1a, 0x49, 0x7d, 0xfb, 0x6c, 0x75, 0x78, 0x5d, 0x49, 0x59, 0xd5, 0xb8, 0xb4, 0x5c, 0xf6,
+	0xbb, 0x25, 0x36, 0x9d, 0x79, 0x1b, 0xaf, 0xe2, 0x4f, 0x02, 0xfe, 0x7a, 0xd4, 0xd1, 0x73, 0xf0,
+	0xf2, 0x8c, 0x91, 0x88, 0x24, 0xb3, 0xc2, 0xcb, 0x33, 0x7a, 0x09, 0xf3, 0x8d, 0x46, 0x95, 0x67,
+	0xcc, 0xb3, 0x33, 0x47, 0x94, 0x81, 0xcf, 0x85, 0x50, 0xa8, 0x35, 0x9b, 0x45, 0x24, 0x39, 0x2d,
+	0xbe, 0x71, 0x50, 0x3c, 0x4a, 0xf5, 0xc2, 0x0d, 0x3b, 0xb6, 0x0b, 0x47, 0x34, 0x82, 0xb3, 0xf5,
+	0xb3, 0x6c, 0xf1, 0xa1, 0x6f, 0x4a, 0x54, 0xec, 0xc4, 0x2e, 0x7f, 0x8f, 0x06, 0xe5, 0x0a, 0x5b,
+	0x81, 0x8a, 0xcd, 0x47, 0xe5, 0x48, 0x34, 0x84, 0x20, 0xd7, 0x7c, 0x6b, 0xf6, 0xaf, 0xc8, 0xfc,
+	0x88, 0x24, 0x41, 0x71, 0xe0, 0xf8, 0x83, 0xc0, 0xd5, 0x10, 0x69, 0x85, 0xc6, 0x55, 0xe0, 0xaa,
+	0xea, 0x1b, 0x6c, 0x8d, 0xfe, 0x8f, 0x5d, 0x6e, 0xde, 0x3d, 0x08, 0x5c, 0x50, 0x4d, 0xef, 0x00,
+	0x7e, 0x72, 0xd3, 0x28, 0x75, 0xbf, 0x95, 0xfe, 0x51, 0x28, 0xbc, 0x38, 0x5c, 0xb8, 0x55, 0x7c,
+	0x34, 0x78, 0x70, 0x21, 0xa6, 0x79, 0xdc, 0xc3, 0x62, 0xd3, 0x89, 0x27, 0x83, 0x93, 0x6d, 0x32,
+	0xac, 0x71, 0xa2, 0x4d, 0x39, 0xb7, 0xa3, 0xdb, 0xaf, 0x00, 0x00, 0x00, 0xff, 0xff, 0x1c, 0x40,
+	0xf6, 0x16, 0xcc, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -228,180 +228,180 @@ var _ grpc.ClientConn
 // is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion4
 
-// UsersClient is the client API for Users service.
+// ProfilesClient is the client API for Profiles service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
-type UsersClient interface {
+type ProfilesClient interface {
 	GetProfile(ctx context.Context, in *UserGetProfileArguments, opts ...grpc.CallOption) (*Profile, error)
 	AddProfile(ctx context.Context, in *UserGetProfileArguments, opts ...grpc.CallOption) (*Profile, error)
 	UpdateProfile(ctx context.Context, in *UserGetProfileArguments, opts ...grpc.CallOption) (*Profile, error)
 	DeleteProfile(ctx context.Context, in *UserGetProfileArguments, opts ...grpc.CallOption) (*Profile, error)
 }
 
-type usersClient struct {
+type profilesClient struct {
 	cc *grpc.ClientConn
 }
 
-func NewUsersClient(cc *grpc.ClientConn) UsersClient {
-	return &usersClient{cc}
+func NewProfilesClient(cc *grpc.ClientConn) ProfilesClient {
+	return &profilesClient{cc}
 }
 
-func (c *usersClient) GetProfile(ctx context.Context, in *UserGetProfileArguments, opts ...grpc.CallOption) (*Profile, error) {
+func (c *profilesClient) GetProfile(ctx context.Context, in *UserGetProfileArguments, opts ...grpc.CallOption) (*Profile, error) {
 	out := new(Profile)
-	err := c.cc.Invoke(ctx, "/protocs.Users/GetProfile", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/protocs.Profiles/GetProfile", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *usersClient) AddProfile(ctx context.Context, in *UserGetProfileArguments, opts ...grpc.CallOption) (*Profile, error) {
+func (c *profilesClient) AddProfile(ctx context.Context, in *UserGetProfileArguments, opts ...grpc.CallOption) (*Profile, error) {
 	out := new(Profile)
-	err := c.cc.Invoke(ctx, "/protocs.Users/AddProfile", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/protocs.Profiles/AddProfile", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *usersClient) UpdateProfile(ctx context.Context, in *UserGetProfileArguments, opts ...grpc.CallOption) (*Profile, error) {
+func (c *profilesClient) UpdateProfile(ctx context.Context, in *UserGetProfileArguments, opts ...grpc.CallOption) (*Profile, error) {
 	out := new(Profile)
-	err := c.cc.Invoke(ctx, "/protocs.Users/UpdateProfile", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/protocs.Profiles/UpdateProfile", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-func (c *usersClient) DeleteProfile(ctx context.Context, in *UserGetProfileArguments, opts ...grpc.CallOption) (*Profile, error) {
+func (c *profilesClient) DeleteProfile(ctx context.Context, in *UserGetProfileArguments, opts ...grpc.CallOption) (*Profile, error) {
 	out := new(Profile)
-	err := c.cc.Invoke(ctx, "/protocs.Users/DeleteProfile", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/protocs.Profiles/DeleteProfile", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// UsersServer is the server API for Users service.
-type UsersServer interface {
+// ProfilesServer is the server API for Profiles service.
+type ProfilesServer interface {
 	GetProfile(context.Context, *UserGetProfileArguments) (*Profile, error)
 	AddProfile(context.Context, *UserGetProfileArguments) (*Profile, error)
 	UpdateProfile(context.Context, *UserGetProfileArguments) (*Profile, error)
 	DeleteProfile(context.Context, *UserGetProfileArguments) (*Profile, error)
 }
 
-// UnimplementedUsersServer can be embedded to have forward compatible implementations.
-type UnimplementedUsersServer struct {
+// UnimplementedProfilesServer can be embedded to have forward compatible implementations.
+type UnimplementedProfilesServer struct {
 }
 
-func (*UnimplementedUsersServer) GetProfile(ctx context.Context, req *UserGetProfileArguments) (*Profile, error) {
+func (*UnimplementedProfilesServer) GetProfile(ctx context.Context, req *UserGetProfileArguments) (*Profile, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetProfile not implemented")
 }
-func (*UnimplementedUsersServer) AddProfile(ctx context.Context, req *UserGetProfileArguments) (*Profile, error) {
+func (*UnimplementedProfilesServer) AddProfile(ctx context.Context, req *UserGetProfileArguments) (*Profile, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method AddProfile not implemented")
 }
-func (*UnimplementedUsersServer) UpdateProfile(ctx context.Context, req *UserGetProfileArguments) (*Profile, error) {
+func (*UnimplementedProfilesServer) UpdateProfile(ctx context.Context, req *UserGetProfileArguments) (*Profile, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method UpdateProfile not implemented")
 }
-func (*UnimplementedUsersServer) DeleteProfile(ctx context.Context, req *UserGetProfileArguments) (*Profile, error) {
+func (*UnimplementedProfilesServer) DeleteProfile(ctx context.Context, req *UserGetProfileArguments) (*Profile, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method DeleteProfile not implemented")
 }
 
-func RegisterUsersServer(s *grpc.Server, srv UsersServer) {
-	s.RegisterService(&_Users_serviceDesc, srv)
+func RegisterProfilesServer(s *grpc.Server, srv ProfilesServer) {
+	s.RegisterService(&_Profiles_serviceDesc, srv)
 }
 
-func _Users_GetProfile_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Profiles_GetProfile_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(UserGetProfileArguments)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(UsersServer).GetProfile(ctx, in)
+		return srv.(ProfilesServer).GetProfile(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/protocs.Users/GetProfile",
+		FullMethod: "/protocs.Profiles/GetProfile",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(UsersServer).GetProfile(ctx, req.(*UserGetProfileArguments))
+		return srv.(ProfilesServer).GetProfile(ctx, req.(*UserGetProfileArguments))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Users_AddProfile_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Profiles_AddProfile_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(UserGetProfileArguments)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(UsersServer).AddProfile(ctx, in)
+		return srv.(ProfilesServer).AddProfile(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/protocs.Users/AddProfile",
+		FullMethod: "/protocs.Profiles/AddProfile",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(UsersServer).AddProfile(ctx, req.(*UserGetProfileArguments))
+		return srv.(ProfilesServer).AddProfile(ctx, req.(*UserGetProfileArguments))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Users_UpdateProfile_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Profiles_UpdateProfile_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(UserGetProfileArguments)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(UsersServer).UpdateProfile(ctx, in)
+		return srv.(ProfilesServer).UpdateProfile(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/protocs.Users/UpdateProfile",
+		FullMethod: "/protocs.Profiles/UpdateProfile",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(UsersServer).UpdateProfile(ctx, req.(*UserGetProfileArguments))
+		return srv.(ProfilesServer).UpdateProfile(ctx, req.(*UserGetProfileArguments))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-func _Users_DeleteProfile_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Profiles_DeleteProfile_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(UserGetProfileArguments)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(UsersServer).DeleteProfile(ctx, in)
+		return srv.(ProfilesServer).DeleteProfile(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/protocs.Users/DeleteProfile",
+		FullMethod: "/protocs.Profiles/DeleteProfile",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(UsersServer).DeleteProfile(ctx, req.(*UserGetProfileArguments))
+		return srv.(ProfilesServer).DeleteProfile(ctx, req.(*UserGetProfileArguments))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-var _Users_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "protocs.Users",
-	HandlerType: (*UsersServer)(nil),
+var _Profiles_serviceDesc = grpc.ServiceDesc{
+	ServiceName: "protocs.Profiles",
+	HandlerType: (*ProfilesServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
 			MethodName: "GetProfile",
-			Handler:    _Users_GetProfile_Handler,
+			Handler:    _Profiles_GetProfile_Handler,
 		},
 		{
 			MethodName: "AddProfile",
-			Handler:    _Users_AddProfile_Handler,
+			Handler:    _Profiles_AddProfile_Handler,
 		},
 		{
 			MethodName: "UpdateProfile",
-			Handler:    _Users_UpdateProfile_Handler,
+			Handler:    _Profiles_UpdateProfile_Handler,
 		},
 		{
 			MethodName: "DeleteProfile",
-			Handler:    _Users_DeleteProfile_Handler,
+			Handler:    _Profiles_DeleteProfile_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
