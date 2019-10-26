a3b2f03c41e58cddaa6d7fd6f8874953995be4ea
sjuliper7 <sjuliper7@gmail.com>
2019-10-26 16:46:12 +0700

    [sjuliper7] create and completing model for profile

--- services/profile-service/models/profile.go
+++ services/profile-service/models/profile.go
@@ -1,1 +1,10 @@
 package models
+
+type Profile struct {
+	ID          uint64 `db:"id"`
+	UserId      uint64 `db:"user_id"`
+	Address     string `db:"address"`
+	WorkAt      string `db:"work_at"`
+	PhoneNumber string `db:"phone_number"`
+	Gender      string `db:"gender"`
+}
