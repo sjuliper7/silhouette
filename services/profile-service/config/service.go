3c4d02248b22c82a0fe75f7542ff7e21e6ba4f78
sjuliper7 <sjuliper7@gmail.com>
2019-10-26 16:41:53 +0700

    [sjuliper7] completing service

--- services/profile-service/config/service.go
+++ services/profile-service/config/service.go
@@ -1,27 +1,32 @@
 package config
 
 import (
+	"github.com/sjuliper7/silhouette/common/config"
 	"github.com/sjuliper7/silhouette/common/protocs"
+	"github.com/sjuliper7/silhouette/services/profile-service/delivery"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+
+	"github.com/sjuliper7/silhouette/services/profile-service/repositories"
+	"github.com/sjuliper7/silhouette/services/profile-service/usecase"
 )
 
 func (cf *Config) initService() {
-	//repo := repositories.NewMysqlRepository(cg.DB)
-	//usecase := usecase.NewUserUsecase(repo)
+	repo := repositories.NewMysqlRepository(cf.DB)
+	usecase := usecase.NewProfileUsecase(repo)
+
+	svr := grpc.NewServer()
+	profileServer := delivery.NewProfileServer(usecase)
 	//
-	//svr := grpc.NewServer()
-	//userServer := delivery.NewUserServer(usecase)
-	//
-	//protocs.RegisterUsersServer(svr, userServer)
-	//log.Println("Starting RPC server at", config.SERVICE_USER_PORT)
-	//
-	////next running the to http
-	//net, err := net.Listen("tcp", config.SERVICE_USER_PORT)
-	//if err != nil {
-	//	log.Fatalf("could not listen to %s: %v", config.SERVICE_USER_PORT, err)
-	//}
-	//
-	//log.Fatalln(svr.Serve(net))
+	protocs.RegisterProfilesServer(svr, profileServer)
+	log.Println("Starting RPC server at", config.SERVICE_PROFILE_PORT)
+
+	//next running the to http
+	net, err := net.Listen("tcp", config.SERVICE_PROFILE_PORT)
+	if err != nil {
+		log.Fatalf("could not listen to %s: %v", config.SERVICE_PROFILE_PORT, err)
+	}
+
+	log.Fatalln(svr.Serve(net))
 }
