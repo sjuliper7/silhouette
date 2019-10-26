d5894d53f03b857d27ad8033279d00c0b8a980a7
sjuliper7 <sjuliper7@gmail.com>
2019-10-26 16:44:41 +0700

    [sjuliper7] create and completing function grpc

--- services/profile-service/delivery/grpc.go
+++ services/profile-service/delivery/grpc.go
@@ -1,1 +1,11 @@
 package delivery
+
+import "github.com/sjuliper7/silhouette/services/profile-service/usecase"
+
+type ProfileServer struct {
+	usecase usecase.ProfileUsecase
+}
+
+func NewProfileServer(profileUsecase usecase.ProfileUsecase) ProfileServer {
+	return ProfileServer{usecase: profileUsecase}
+}
