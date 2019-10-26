3c608dde21fcc52eaaf04901d0fe2a250656f43e
sjuliper7 <sjuliper7@gmail.com>
2019-10-26 16:53:21 +0700

    [sjuliper7] [update] add service profile port

--- common/config/config.go
+++ common/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 const (
-	SERVICE_USER_PORT = ":9000"
-	REST_USER_PORT    = ":8000"
+	SERVICE_USER_PORT    = ":9000"
+	SERVICE_PROFILE_PORT = ":8888"
+	REST_USER_PORT       = ":8000"
 )
