e6e474f61ec3ec023b23055651587aaa5f677b32
sjuliper7 <sjuliper7@gmail.com>
2019-10-28 12:17:30 +0700

    [sjuliper7] add ProfileRepository interface

--- services/user-service/repositories/repository.go
+++ services/user-service/repositories/repository.go
@@ -5,10 +5,14 @@ import (
 )
 
 //Repository declaration type interface
-type Repository interface {
+type UserRepository interface {
 	GetAlluser() (users []models.User, err error)
 	AddUser(user *models.User) (err error)
 	GetUser(userID int64) (user models.User, err error)
 	UpdateUser(user *models.User) (err error)
-	DeleteUser(userID int64) (deleted bool,err error)
+	DeleteUser(userID int64) (deleted bool, err error)
+}
+
+type ProfileRepository interface {
+	GetProfile(UserID int64) (profile models.Profile, err error)
 }
