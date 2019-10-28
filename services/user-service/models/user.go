87d198ec3c885e2240c445264d615ba531d20b0f
sjuliper7 <sjuliper7@gmail.com>
2019-10-28 12:14:37 +0700

    [sjuliper7] add field profile

--- services/user-service/models/user.go
+++ services/user-service/models/user.go
@@ -2,9 +2,10 @@ package models
 
 //User struct is represntative form table user
 type User struct {
-	ID       uint64 `json:"id" db:"id"`
-	Username string `json:"username" db:"username"`
-	Email    string `json:"email" db:"email"`
-	Name     string `json:"name" db:"name"`
-	Role     string `json:"role" db:"role"`
+	ID       uint64  `json:"id" db:"id"`
+	Username string  `json:"username" db:"username"`
+	Email    string  `json:"email" db:"email"`
+	Name     string  `json:"name" db:"name"`
+	Role     string  `json:"role" db:"role"`
+	Profile  Profile `json:"profile"`
 }
