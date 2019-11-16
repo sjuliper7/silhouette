0a1f99833febb6116e726ead010c4bc1aed8dbcc
sjuliper7 <sjuliper7@gmail.com>
2019-11-16 15:26:56 +0700

    completing kafka product message

--- services/user-service/repositories/kafka/kafka_repository.go
+++ services/user-service/repositories/kafka/kafka_repository.go
@@ -1,9 +1,38 @@
 package kafka
 
 import (
+	"encoding/json"
+	"github.com/sirupsen/logrus"
+	"github.com/sjuliper7/silhouette/services/user-service/models"
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
 )
 
-type kafkaProducer struct {
+type kafkaProducerRepository struct {
 	kafkaRepo *kafka.Producer
+}
+
+func (kRepo kafkaProducerRepository) RegisterDonePublishMessage(profile models.Profile) (err error) {
+	defer kRepo.kafkaRepo.Close()
+
+	topic := "kafka.registration"
+
+	message, err := json.Marshal(profile)
+	if err != nil {
+		logrus.Println("Failed when parse object to json")
+		return err
+	}
+
+	//go routine to handle for produce message
+	go func() {
+		err := kRepo.kafkaRepo.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          []byte(message),
+		}, nil)
+
+		if err != nil {
+			logrus.Println("Error when Publish message")
+		}
+	}()
+
+	return nil
 }
