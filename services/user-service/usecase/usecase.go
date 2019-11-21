94825a6f6903ff0604b9596bb85480ef07c95993
sjuliper7 <sjuliper7@gmail.com>
2019-11-21 15:02:50 +0700

    update params function

--- services/user-service/usecase/usecase.go
+++ services/user-service/usecase/usecase.go
@@ -5,8 +5,8 @@ import "github.com/sjuliper7/silhouette/services/user-service/models"
 //UserUsecase interface declaration
 type UserUsecase interface {
 	GetAlluser() (users []models.User, err error)
-	AddUser(user *models.User) (err error)
+	AddUser(user *models.UserTable) (err error)
 	GetUser(userID int64) (user models.User, err error)
-	UpdateUser(user *models.User) (err error)
-	DeleteUser(userID int64) (deleted bool,err error)
+	UpdateUser(user *models.UserTable) (err error)
+	DeleteUser(userID int64) (deleted bool, err error)
 }
