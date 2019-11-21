d416e8f82edde9fcc13347d72586d431e9e8292f
sjuliper7 <sjuliper7@gmail.com>
2019-11-21 14:57:35 +0700

    update model and add new model

--- services/user-service/models/user.go
+++ services/user-service/models/user.go
@@ -1,11 +1,26 @@
 package models
 
-//User struct is represntative form table user
+import "time"
+
+//User struct is return value as response
 type User struct {
-	ID       uint64  `json:"id" db:"id"`
-	Username string  `json:"username" db:"username"`
-	Email    string  `json:"email" db:"email"`
-	Name     string  `json:"name" db:"name"`
-	Role     string  `json:"role" db:"role"`
-	Profile  Profile `json:"profile"`
+	ID        uint64    `json:"id" db:"id"`
+	Username  string    `json:"username" db:"username"`
+	Email     string    `json:"email" db:"email"`
+	Name      string    `json:"name" db:"name"`
+	Role      string    `json:"role" db:"role"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Profile   Profile   `json:"profile"`
+}
+
+//User struct is representative form table user
+type UserTable struct {
+	ID        uint64    `db:"id"`
+	Username  string    `db:"username"`
+	Email     string    `db:"email"`
+	Name      string    `db:"name"`
+	Role      string    `db:"role"`
+	CreatedAt time.Time `db:"created_at"`
+	UpdatedAt time.Time `db:"updated_at"`
 }
