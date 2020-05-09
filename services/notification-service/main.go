38a3fd2ebf191d0cf288f57f7c5d0cc51f57960f
juliper <juliper.simanjuntak@qoala.id>
2020-05-09 12:49:55 +0700

    [NotifictionService] continue completing configuration

--- services/notification-service/main.go
+++ services/notification-service/main.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+)
+
+func loadConfig() {
+	err := godotenv.Load()
+	if err != nil {
+		logrus.Fatal("error loading .env file")
+	} else {
+		logrus.Info("env has been loaded successfully..")
+	}
+
+}
+
+//
+func init() {
+	formatter := &logrus.TextFormatter{
+		FullTimestamp: true,
+	}
+	logrus.SetFormatter(formatter)
+	loadConfig()
+}
+
+//
+func main() {
+	//start service
+	fmt.Println("Hello World")
+}
--- services/notification-service/usecase/notication_usecase.go
+++ services/notification-service/usecase/notication_usecase.go
@@ -1,16 +1,38 @@
 package usecase
 
-import "github.com/sjuliper7/silhouette/services/notification-service/model"
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/sjuliper7/silhouette/services/notification-service/model"
+	"github.com/sjuliper7/silhouette/services/notification-service/repository"
+)
+
+const (
+	email = "email"
+)
 
 type notificationUsecase struct {
+	emailRepository repository.EmailRepository
 }
 
 //NewNotificatonUsecase ...
-func NewNotificatonUsecase() NotificationUsecase {
-	return &notificationUsecase{}
+func NewNotificatonUsecase(emailRepository repository.EmailRepository) NotificationUsecase {
+	return &notificationUsecase{emailRepository: emailRepository}
 }
 
 func (notificationCase *notificationUsecase) AccountRegisterNotification(notif model.Notification) error {
+	if notif.Type == email {
+		email := model.Email{}
+		email.Receivers = []string{notif.Email}
+		email.Sender = ""
+		email.Subject = "Welcome Dude"
+		email.Message = `Hello, <br> <b>have a nice data<b/> <br> Thank you for join to our system!`
+
+		err := notificationCase.emailRepository.SentEmail(email)
+		if err != nil {
+			logrus.Infof("[usecase][AccountRegisterNotification] failed when access SentEmail %v", err)
+			return err
+		}
+	}
 	return nil
 }
 
